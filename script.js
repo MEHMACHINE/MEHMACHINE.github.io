393e78810917bf9c583afda43cea0860c3cb2a09
Ashton <ashalexander412@gmail.com>
2025-09-10 10:26:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillRect(0,0,50,50);
+function init() {
+  // This is where you import all images and such
+  square_pos_x = 0;
+
+  window.requestAnimationFrame(draw);
+
+}
+
+function draw() {
+  // Draw
+  ctx.clearRect(0,0,400,400);
+
+  square_pos_x += 10;
+  ctx.fillRect(square_pos_x,0,50,50);
+
+  window.requestAnimationFrame(draw);
+
+}
