a8ec5646fe4490832cb911c077b865c1d70af84b
Ashton <ashalexander412@gmail.com>
2025-09-11 22:06:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
-ctx.translate(width/2, height/2);
+ctx.translate(width/2, height/2); // Move the origin
 
-// Draw a rectangle
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0 , 100, 50);
+function gameLoop() {
+  // Words
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 100, 100);
 
-// Draw a word
-ctx.fillStyle = "red";
-ctx.font = "48px georgia";
-ctx.textFill("Sample Text", width/2, 0);
+  requestAnimationFrame(gameLoop); // FROM THE TOP!!
+}
+
+gameLoop(); // Initial entrace to the game
