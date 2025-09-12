be0e096a8e9b953c0320653b60155ca39c8593da
Ashton <ashalexander412@gmail.com>
2025-09-11 21:55:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
+ctx.translate(width/2, height/2);
+
 // Draw a rectangle
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0 , 100, 50);
