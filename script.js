a493333ea297d6647cb3f87607e8ec6ef5de988e
Ashton <ashalexander412@gmail.com>
2025-09-11 22:14:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,29 @@ const ctx = canvas.getContext("2d");
 
 ctx.translate(width/2, height/2); // Move the origin
 
+var xpos = 0;
+var ypos = 0;
+
 function gameLoop() {
   // Words
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, 100, 100);
 
+  update();
+  draw();
+
   requestAnimationFrame(gameLoop); // FROM THE TOP!!
+}
+
+function update() {
+  // stuff
+  xpos += 5;
+}
+
+function draw () {
+  ctx.fillStyle = "white";
+  ctx.fillRect(xpos, ypos, xpos + 50, ypos + 50);
+  // Draw
 }
 
 gameLoop(); // Initial entrace to the game
