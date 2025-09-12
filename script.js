48851957d2808b8718adcb80aaa16d04f677a18f
Ashton <ashalexander412@gmail.com>
2025-09-11 21:45:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-// I don't know what to put here
-
+// Make the canvas and define size
 const canvas = document.querySelector(".myCanvas");
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
-
 const ctx = canvas.getContext("2d");
 
+// Draw a rectangle
 ctx.fillStyle = "black";
-ctx.fillRect(0,0,100,50);
+ctx.fillRect(0, 0 , 100, 50);
+
+// Draw a word
+ctx.fillStyle = "red";
+ctx.font = "48px georgia";
+ctx.textFill("Sample Text", width/2, 0);
