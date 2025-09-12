09a408d014539ff8b7ae9d8e2204036fafc5a08b
Ashton <ashalexander412@gmail.com>
2025-09-11 22:50:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,38 @@ ctx.translate(width/2, height/2); // Move the origin
 var xpos = 0;
 var ypos = 0;
 
+window.addEventListener(
+  "keydown",
+  (event) => {
+    if (event.defaultPrevented) {
+      return; // Do nothing if the event was already processed
+    }
+
+    switch (event.key) {
+      case "ArrowDown":
+        // Do something for "down arrow" key press.
+        break;
+      case "ArrowUp":
+        // Do something for "up arrow" key press.
+        break;
+      case "ArrowLeft":
+        // Do something for "left arrow" key press.
+        xpos--;
+        break;
+      case "ArrowRight":
+        // Do something for "right arrow" key press.
+        xpos++;
+        break;
+      default:
+        return; // Quit when this doesn't handle the key event.
+    }
+
+    // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
+  },
+  true,
+);
+
 function gameLoop() {
   // Words
   ctx.fillStyle = "black";
